3d542a6cd8a23751537ded741cb393988c7397f5
Shaun Wakashige <swak@theswak.com>
2024-02-03 21:36:49 -0800

    added heroes component

---
+++ angular-tour-of-heroes/src/app/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HeroesComponent } from '../heroes/heroes.component';
+import { AppComponent } from '../app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent
+  ],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    NgIf,
+    NgFor
+  ],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
+  heroes = HEROES;
+  selectedHero?: Hero;
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
