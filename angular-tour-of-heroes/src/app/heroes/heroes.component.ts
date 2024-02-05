36f024c3d768225e717059ee6e55949d3acb1298
Shaun Wakashige <swak@theswak.com>
2024-02-05 12:49:08 -0800

    first step of setting up a service.

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NgFor } from '@angular/common';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 
 @Component({
@@ -16,11 +16,21 @@ import { HEROES } from '../mock-heroes';
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = [];
 
   selectedHero!: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  constructor(private heroService: HeroService) {}
 }
