a0f432e24bda0e1b1eb9983547e7b24925835be2
Shaun Wakashige <swak@theswak.com>
2024-02-05 14:26:24 -0800

    adding services but no visual

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,36 +1,32 @@
-import { Component } from '@angular/core';
-import { NgFor } from '@angular/common';
-import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 
 @Component({
   selector: 'app-heroes',
-  standalone: true,
-  imports: [
-    HeroDetailComponent,
-    NgFor
-  ],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
+
+  selectedHero?: Hero;
+
   heroes: Hero[] = [];
 
-  selectedHero!: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-  getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
-  }
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  constructor(private heroService: HeroService) {}
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add('HerosComponent: Selected hero id=${hero.id}');
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
 }
